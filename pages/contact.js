1ea7b7a5663ba4783068d4046ee84dfcac7dbc00
Michael Brantley <brantleyrecs@gmail.com>
2024-04-09 19:53:13 -0500

    started contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div className="text-center d-flex flex-column">
+        <h1>Contact</h1>
+        <p>EMAIL GOES HERE</p>
+      </div>
+    </>
+  );
 }
