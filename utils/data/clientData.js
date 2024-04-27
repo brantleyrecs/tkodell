1050762887ca53521f44cf3a987df62f99e5f653
Michael Brantley <brantleyrecs@gmail.com>
2024-04-27 11:05:51 -0500

    create client promise created

--- utils/data/clientData.js
+++ utils/data/clientData.js
@@ -32,4 +32,17 @@ const getSingleClient = (firebaseKey) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export { getClients, getSingleClient };
+const createClient = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/clients.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export { getClients, getSingleClient, createClient };
